0d6dcc99bff93db2e24d1e80694c906bbe31554c
Zabuka_zuzu <zuzu950444@gmail.com>
2026-02-28 19:59:18 +0000

    build: Add build info and fix ro.product.name

--- tools/releasetools/non_ab_ota.py
+++ tools/releasetools/non_ab_ota.py
@@ -214,6 +214,37 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
+  # Build info
+    script.Print("-----------------------------------------------");
+  script.Print("         _      _   _  _____     ____    _____ ");
+  script.Print("        | |    | \ | |/ ____|   / __ \  / ____|");
+  script.Print("        | |    |  \| | |  __   | |  | |( (___  ");
+  script.Print("        | |    | . ` | | |_ |  | |  | | \___ \ ");
+  script.Print("        | |____| |\  | |__| |  | |__| | ____) |");
+  script.Print("        |______|_| \_|\_____|   \____/ |_____/ ");
+  script.Print("-----------------------------------------------");
+
+
+  buildid = target_info.GetBuildProp("ro.lightning.version_display")
+  androidver = target_info.GetBuildProp("ro.build.version.release")
+  buildidn = target_info.GetBuildProp("ro.build.id")
+  buildday = target_info.GetBuildProp("ro.build.date")
+  securep = target_info.GetBuildProp("ro.build.version.security_patch")
+  device = target_info.GetBuildProp("ro.product.name")
+  manufacturer = target_info.GetBuildProp("ro.product.manufacturer")
+  maintainer = target_info.GetBuildProp("ro.lightningos.maintainer")
+  script.Print("***********************************************");
+  script.Print(" ROM version      : %s"%(buildid));
+  script.Print(" Android version  : %s"%(androidver));
+  script.Print(" Security patch   : %s"%(securep));
+  script.Print(" Build date       : %s"%(buildday));
+  script.Print("***********************************************");
+  script.Print(" Device           : %s"%(device));
+  script.Print(" Manufacturer     : %s"%(manufacturer));
+  script.Print(" Maintainer       : %s"%(maintainer));
+  script.Print("***********************************************");
+  script.Print("                                         ");
+
   device_specific.FullOTA_InstallBegin()
 
   CopyInstallTools(output_zip)
